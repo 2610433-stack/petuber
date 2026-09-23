785dca45b221984124cf7955765a4a41959ae279
Kelly Mello <2610433@unicesusc.edu.br>
2026-09-23 15:26:32 -0300

    Hoje não vamos ter aula presencial Ao todo temos 5 bichinhos em casa

--- petpython.py
+++ petpython.py
@@ -1,5 +1,6 @@
 print('Hoje não vamos ter aula presencial')
 cachorros=2
 gatos=3
-total_animais=cahorros+gatos
-print(total)
+total_animais=(cachorros)+(gatos)
+print(f'Ao todo temos {total_animais} bichinhos em casa')
+
